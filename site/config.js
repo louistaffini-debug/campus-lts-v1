b72b39905ec61b439290dd2b3146ea28e992865c
louistaffini-debug <louis.taffini@lycee-les-eucalyptus.org>
2026-09-22 16:10:27 +0200

    Relier le site à l’adresse publique de l’API protégée

--- site/config.js
+++ site/config.js
@@ -1,4 +1,5 @@
 // Configuration publique uniquement. Ne jamais placer une clé Grist ou un code ici.
 window.CAMPUS_CONFIG = Object.freeze({
-  apiUrl: 'https://script.google.com/a/macros/lycee-les-eucalyptus.org/s/AKfycbx4TDNMLQFvRB34IFNG4r_MMeQfTIWBA-5De5YlTgf-J3ZIZN1JIP0ltvifGXTkuY6J/exec'
+  apiUrl: 'https://script.google.com/macros/s/AKfycbx4TDNMLQFvRB34IFNG4r_MMeQfTIWBA-5De5YlTgf-J3ZIZN1JIP0ltvifGXTkuY6J/exec'
 });
+
